37ec272ab23c8ebfc09966460c52d16b92c448af
Francisco Carmona <86492879+jcarmfran@users.noreply.github.com>
2024-06-22 17:37:43 +0000

    Refactor to use variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,63 @@
+##############################
+### INSTANCE TYPE VARIABLE ###
+##############################
+
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
+}
+
+
+####################
+### AMI VARIABLE ###
+####################
+
+variable "ami_filter" "app_ami" {
+  description = "Name filter and owner for AMI"
+
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default = {
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owners = "979382823631" # Bitnami
+  }
+}
+
+
+############################
+### ENVIRONMENT VARIABLE ###
+############################
+
+variable "environment" {
+  description = "Development Environment"
+
+  type = object ({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
+  }
+}
+
+
+#############################
+### AUTOSCALING VARIABLES ###
+#############################
+
+### MINIMUM ###
+variable "asg_min_size" {
+  description = "Minimum number of instances in the ASG"
+  default = 1
+}
+
+### MAXIMUM ###
+variable "asg_max_size" {
+  description = "Maximum number of instances in the ASG"
+  default = 2
 }
